01442cf101822eb62ba28075890e284cbbf1b105
Pratham <85452206+PrathamWasTaken@users.noreply.github.com>
2023-01-03 01:46:48 +0530

    [🐱‍🏍] Refactored the code...
    Got rid of unnecessary comments and used list comprehension to extract the order from the CSV file, instead of looping through the file.

--- super-merge.py
+++ super-merge.py
@@ -1,34 +1,16 @@
 import os
 import csv
 
+def main():
+    with open("super_map.csv", "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        order = [line[2:] for line in reader if "IN" in line]
+        order.append("temp-super.img")
+        names = " ".join(order)
+
+        os.system(f"simg2img {names}")
+
 if __name__ == "__main__":
-    # TO DO(here): Make it take args for the region
-
-    with open("super_map.csv", "r") as csv_file:
-        csv_reader = csv.reader(csv_file)
-
-        next(csv_reader)
-
-        for line in csv_reader:
-            if "IN" in line:
-                super_order = line[2:]
-
-        supers = " ".join(super_order)  # IT WAS JUST 1 F*CKING FUNCTION
-        # Idk python brev, eitherway leaving the insane shit i wrote
-        # below, for you to laugh at :(
-
-        # TO DO(here): Make this for loop cleaner with 1 liners?? DONE
-
-        # NOTE: almost lost my sanity here, since i had people on discord helping me
-        #       make it efficient... But thanks to them its just 1 line now...
-
-        # for super_name in super_order:
-        #     supers = f"{super_name}" if not supers else f"{supers} {super_name}"
-        # if supers == "":
-        #     supers = f"{super}"
-        # else:
-        #     supers = f"{supers} {super}"
-
-        supers = f"{supers} temp-super.img"
-
-        os.system(f"simg2img {supers}")
+    main()
